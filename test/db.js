d69b24e3ceccac7ac5382170fd9084e4db6ade3e
Jakob Borg <jakob@nym.se>
2012-03-25 19:38:12 +0200

    db.client uses callback instead of promise.

--- test/db.js
+++ test/db.js
@@ -3,12 +3,8 @@ var should = require('should');
 
 describe('db', function () {
     describe('client', function () {
-        it('should return a promise of a client', function() {
-            should.exist(db.client());
-        });
-        it('should fulfil the promise of a client', function(done) {
-            var c = db.client();
-            c.then(function (client) {
+        it('should produce a client', function(done) {
+            db.client(function (err, client) {
                 should.exist(client);
                 done();
             });
@@ -17,13 +13,13 @@ describe('db', function () {
 
     describe('insert', function () {
         it('should return a promise of a doc', function(done) {
-            db.client().then(function (c) {
+            db.client(function (err, c) {
                 should.exist(db.insert(c, 'test-collection', {foo: 'bar'}));
                 done();
             });
         });
         it('should fulfil the promise of a doc after insert', function(done) {
-            db.client().then(function (c) {
+            db.client(function (err, c) {
                 db.insert(c, 'test-collection', {foo: 'bar'}).then(function (docs) {
                     docs.should.have.length(1);
                     docs[0].should.have.property('foo');
@@ -37,13 +33,13 @@ describe('db', function () {
 
     describe('update', function () {
         it('should return a promise of a doc', function(done) {
-            db.client().then(function (c) {
+            db.client(function (err, c) {
                 should.exist(db.update(c, 'test-collection', {foo: 'bar'}));
                 done();
             });
         });
         it('should fulfil the promise of a doc after update', function(done) {
-            db.client().then(function (c) {
+            db.client(function (err, c) {
                 db.update(c, 'test-collection', {foo: 'bar'}, {foo: { $set: 'baz' }}).then(function (status) {
                     status.should.be.true;
                     done();
@@ -54,13 +50,13 @@ describe('db', function () {
 
     describe('find', function () {
         it('should return a promise of a doc', function(done) {
-            db.client().then(function (c) {
+            db.client(function (err, c) {
                 should.exist(db.find(c, 'test-collection', {foo: 'bar'}));
                 done();
             });
         });
         it('should fulfil the promise of a doc after find', function(done) {
-            db.client().then(function (c) {
+            db.client(function (err, c) {
                 db.update(c, 'test-collection', {a: 'a'}, { a: 'a', b: 'b' }).then (function () {
                     db.find(c, 'test-collection', {a: 'a'}).then(function (docs) {
                         should.exist(docs);
